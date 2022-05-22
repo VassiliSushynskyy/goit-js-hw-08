291889b1ad701e13d6bd9f9227219235e235dca5
VasylSushynskyy <92335667+VassiliSushynskyy@users.noreply.github.com>
2022-05-22 23:28:43 +0200

    Update 3 modul

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,41 @@
-const ref = {
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-msg';
+const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('.email'),
   textarea: document.querySelector('.message'),
-  button: document.querySelector('.submit'),
 };
+populateTextarea();
+const formData = {};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 200));
+
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('Отправляем форму');
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
+// const formData = {};
+
+// refs.form.addEventListener('input', e => {
+//   // console.log(e.target.name);
+//   // console.log(e.target.value);
+
+//   formData[e.target.name] = e.target.value;
+
+//   console.log(formData);
+// });
